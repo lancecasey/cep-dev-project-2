49ba84f2f61087bceb87594d82cceb7b8f2de0b0
Lance Casey <lacasey@homeaway.com>
2019-08-12 21:00:52 -0500

    basic search results finished

---
+++ front/src/SearchResults/NoResult/NoResult.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const NoResults = props => {
+  return (
+    <li className='no-results'>
+      <h3>Sorry, no results were found.</h3>
+    </li>
+  )
+}
+
+export default NoResults
