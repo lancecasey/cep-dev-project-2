221be456fe079af73aa55a174d6057f9cc7ef0d3
Lance Casey <lacasey@homeaway.com>
2019-08-12 19:22:01 -0500

    Search Results folder structure

---
+++ front/src/SearchResults/SearchResults.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Result from './Result/Result'
+import NoResult from './NoResult/NoResult'
+
+export default class SearchResults extends Component {
+state = {
+    listings: []
+}
+
+    render() {
+        return (
+            <div>
+                <
+            </div>
+        )
+    }
+
+}
