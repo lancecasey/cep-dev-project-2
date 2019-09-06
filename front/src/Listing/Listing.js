09b5142f730803e4118ac5cf65f12ef3d9252052
Lance Casey <lacasey@homeaway.com>
2019-09-06 11:15:32 -0500

    add fetch to Listing comp, minor formatting change to Review Form comp

--- front/src/Listing/Listing.js
+++ front/src/Listing/Listing.js
@@ -4,37 +4,60 @@ import Header from "../Header/Header"
 import Amenities from "../Amenities"
 import "./styles.css"
 
-const Listing = props => {
-  return (
-    <div class='parent'>
-      <header class='header'>
-        <Header />
-      </header>
-      <div class='grid-container'>
-        <div class='image'>
-          <img src='https://placekitten.com/800/480' alt='listing' />
-        </div>
+const baseListingUri = ""
+class Listing extends React.Component {
+  state = {
+    listing: {},
+    reviews: [],
+    isLoading: false
+  }
 
-        <section>
-          <h1>(property title from props)</h1>
-          <div class='meat'>
-            <div class='main'>
-              <h3>(property location from props)</h3>
-              <p>(property desctipion from props)</p>
-            </div>
-            <div class='amenities'>
-              <Amenities />
-            </div>
+  componentDidMount() {
+    this.setState({ isLoading: true })
+
+    fetch("https://reqres.in/api/users/1")
+      .then(data => data.json())
+      .then(data => {
+        console.log(data)
+        this.setState({
+          listing: data.data,
+          isLoading: false
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div class='parent'>
+        <header class='header'>
+          <Header />
+        </header>
+        <div class='grid-container'>
+          <div class='image'>
+            <img src={this.state.listing.avatar} alt='listing' />
           </div>
-        </section>
 
-        <div class='review'>
-          <h4>How was your stay?</h4>
-          <ReviewForm />
+          <section>
+            <h1>{this.state.listing.first_name}</h1>
+            <div class='meat'>
+              <div class='main'>
+                <h3>{this.state.listing.last_name}</h3>
+                <p>{this.state.listing.email}</p>
+              </div>
+              <div class='amenities'>
+                <Amenities />
+              </div>
+            </div>
+          </section>
+
+          <div class='review'>
+            <h4>How was your stay?</h4>
+            <ReviewForm />
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Listing
